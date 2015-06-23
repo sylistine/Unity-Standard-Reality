cf20bdd2c9653b32a2e1b72091999cac8fabf653
Aaron Hull <aaron@10hpgames.com>
2015-06-24 01:21:32 +0900

    Init Project Commit.

---
+++ Unity Standard Reality/Assets/fps/FPSMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSMovement : MonoBehaviour
+{
+	public float speed;
+	
+	private Camera hmd;
+	private Transform hmdRoot;
+	
+	void Start ()
+	{
+		hmd = Camera.main;
+		hmdRoot = hmd.transform.parent;
+	}
+	
+	void Update ()
+	{
+		float v = Input.GetAxisRaw ("Vertical");
+		float h = Input.GetAxisRaw ("Horizontal");
+		
+		Vector3 rootRight = hmdRoot.TransformDirection (Vector3.right);
+		Vector3 rootForward = new Vector3(-rootRight.z, 0, rootRight.x);
+		
+		this.transform.position += (rootRight * h + rootForward * v).normalized * speed * Time.deltaTime;
+	}
+}
